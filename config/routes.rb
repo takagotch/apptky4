347f636f43013cda69b2006e3f1ec536fc729ada
takagotch <dyaccb@gmail.com>
2020-07-22 11:01:46 +0900

    users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # heroku
+  root 'users#index'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
